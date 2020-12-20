4cae09e1cac66a82f4fc01b1cb7841706570270e
Shuming <shumingx.liu@gmail.com>
2020-12-20 12:07:44 -0600

    fix bug

--- projects/brachio-graph/photo_draw.py
+++ projects/brachio-graph/photo_draw.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     # vectorise an image to json file
     image_to_json(photo_name, draw_contours=2, draw_hatch=16)
     # draw with the plotter
-    bg.plot_file("images/" + photo_name + "a.json")
+    bg.plot_file("images/" + photo_name + ".json")
