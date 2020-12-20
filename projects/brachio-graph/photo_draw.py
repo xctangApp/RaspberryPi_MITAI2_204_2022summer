abe0df0ffcacd859cfa4ff004f1d8f3e6a3603f7
Shuming <shumingx.liu@gmail.com>
2020-12-20 12:03:13 -0600

    add phto draw py

---
+++ projects/brachio-graph/photo_draw.py
@@ -0,0 +1,16 @@
+import sys
+from brachiograph import BrachioGraph
+from linedraw import *
+
+inner_arm = 8
+outer_arm = 8
+
+bg = BrachioGraph(inner_arm, outer_arm)
+
+if __name__ == "__main__":
+
+    photo_name = sys.argv[0]
+    # vectorise an image to json file
+    image_to_json(photo_name, draw_contours=2, draw_hatch=16)
+    # draw with the plotter
+    bg.plot_file("images/" + photo_name + "a.json")
