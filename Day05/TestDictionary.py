e3d1337277afa2b12fcabbd90bcd4364ef06d1ec
xctangApp <99854995+xctangApp@users.noreply.github.com>
2022-07-22 21:10:35 -0500

    Create TestDictionary.py

---
+++ Day05/TestDictionary.py
@@ -0,0 +1,7 @@
+band = {'john': 'guitar','paul': 'bass','batman':'lead guitar'}
+
+#for member in band:
+#    print(member)
+
+for member,instrument in band.items():
+    print(member, '-', instrument)
